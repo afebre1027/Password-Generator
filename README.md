658cdfbe656afeda7aecd6b60a8ecb26d30df886
Andrew Febre <afebre1027@gmail.com>
2021-11-21 20:42:05 -0500

    working

---
+++ README.md
@@ -0,0 +1,11 @@
+# Password Generator week 3 challenge
+### Criteria Met
+* Click button to generate a random password.
+* Length of password between 8 & 128 characters with uppercase, lowercase, numbers, & symbols.
+* display a alert message when password is not between 8 & 128.
+* 
+* 
+
+### URL link 
+* https://github.com/afebre1027/Password-Generator - github repo
+* https://afebre1027.github.io/Password-Generator/ - password generator application
--- script.js
+++ script.js
@@ -11,20 +11,50 @@ var symbols = "!@#$%^&*().,?";
 function generatePassword(){
     console.log("btn");
     // password length between 8 & 128 characters
-    var pswd = "";
-    var pswdChar = "";
 
     var passwordLength = prompt("choice between 8  & 128 characters");
+    passwordLength = parseInt(passwordLength);
 
+    if (passwordLength < 8){
+      alert("password length is to short, try again!");
+      return "";
+    }
+    if (passwordLength > 128){
+      alert("password lenght is to long, try again!")
+      return "";
+    }
     // lowercase, uppercase, number, symbol
-    
-    
-    // validate the input
-    
-    
-    
-    // generate password on page
-    return
+    //random symbol
+function getRandomSymbol() {
+  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+  
+}
+// console.log(getRandomSymbol());
+ window.confirm ("Would you like Symbols?");
+
+ // random number 0-9
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+// console.log(getRandomNumber());
+ window.confirm ("Would you like Numbers?");
+
+// random upper case letters
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+// console.log(getRandomUpper());
+ window.confirm ("Would you like Uppercase Letters?");
+
+// random lower case letters
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+// console.log(getRandomLower());
+ window.confirm ("Would you like Lowercase Letters?");
+
+// Write password to the #password input
+
 }
 // Write password to the #password input
 function writePassword() {
