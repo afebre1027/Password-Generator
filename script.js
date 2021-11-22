0f52a23a75f3a97b7cf450687eabd558b79f7879
Andrew Febre <afebre1027@gmail.com>
2021-11-22 13:59:49 -0500

    completed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 
 function generatePassword(){
-    console.log("btn");
+    // console.log("btn");
     // password length between 8 & 128 characters
 
     var passwordLength = prompt("choice between 8  & 128 characters");
@@ -19,36 +19,57 @@ function generatePassword(){
     }
     // lowercase, uppercase, number, symbol
     //random symbol
+    var confirmSymbol = confirm("would you like symbols?");
+    var confirmNumber = confirm("would you like numbers");
+    var confirmLower = confirm("would you like lowercase");
+    var confirmUpper = confirm("would you like uppercase");
+  var confirmSelection = []
+  var finalPassword = "";
+    if(confirmSymbol === true){
+    confirmSelection.push(getRandomSymbol)
+    finalPassword += getRandomSymbol();
+  }
+    if(confirmNumber === true){
+      confirmSelection.push(getRandomNumber)
+      finalPassword += getRandomNumber();
+    }
+    if(confirmLower === true){
+      confirmSelection.push(getRandomLower)
+      finalPassword += getRandomLower();
+    }
+    if(confirmUpper === true){
+      confirmSelection.push(getRandomUpper)
+      finalPassword += getRandomUpper();
+    }
+   console.log(finalPassword);
+    for (var i = finalPassword.length; i < passwordLength; i++){
+      finalPassword += confirmSelection[Math.floor(Math.random() * confirmSelection.length)]();
+    }
+    return finalPassword;
+// Write password to the #password input
+
+}
 function getRandomSymbol() {
   return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
   
 }
 // console.log(getRandomSymbol());
- window.confirm ("Would you like Symbols?");
 
  // random number 0-9
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 // console.log(getRandomNumber());
- window.confirm ("Would you like Numbers?");
 
 // random upper case letters
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 // console.log(getRandomUpper());
- window.confirm ("Would you like Uppercase Letters?");
 
 // random lower case letters
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-// console.log(getRandomLower());
- window.confirm ("Would you like Lowercase Letters?");
-
-// Write password to the #password input
-
 }
 // Write password to the #password input
 function writePassword() {
